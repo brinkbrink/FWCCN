2003416f2d5d5c1063338b63b9b51d9d3bb9e07d
E. Brink <80282265+brinkbrink@users.noreply.github.com>
2023-04-15 13:25:25 -0700

    reworked bacakend to ensure security

--- server/index.js
+++ server/index.js
@@ -1,19 +1,53 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/user.js"));
-// get driver connection
-const dbo = require("./db/conn.js");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const ApplicantModel = require('./models/Applicants')
 
-app.listen(port, () => {
-    // perform a database connection when server starts
-    dbo.connectToServer(function (err) {
-        if (err) console.error(err);
+dotenv.config()
+// Replace the uri string with your connection string.
+const db_username = process.env.MONGO_DB_USERNAME;
+const db_password = process.env.MONGO_DB_PASSWORD;
+const db_url = process.env.MONGO_DB_URL;
+const uri = `mongodb+srv://${db_username}:${db_password}@${db_url}?retryWrites=true&w=majority`;
 
-    });
-    console.log(`Server is running on port: ${port}`);
+mongoose.connect(uri);
+
+
+//can also use routes for the following
+app.get("/getApplicants", (req, res) => {
+    ApplicantModel.find({}, (err, result) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    })
+})
+
+//TODO: test/fix endpoints
+
+app.post("/createApplicants", async (req, res) => {
+    const applicant = req.body;
+    const newApplicant = new ApplicantModel(applicant);
+    await newApplicant.save();
+})
+
+
+app.get("/getApplicants", async (req,res) => {
+    try {
+        await client.connect();
+        const result = await ApplicantModel.find({});
+        res.type('json');
+        res.status(200);
+        res.json(result);
+    } catch (error) {
+      console.log(error)  
+    } finally {
+        await client.close();
+    }
+});
+
+app.listen(3001, () => {
+    console.log("server is running")
 });
