d2d6a2bef5800c41c414ab5e025b5b06046d163b
E. Brink <80282265+brinkbrink@users.noreply.github.com>
2023-06-02 11:23:01 -0700

    routes

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function NotFound () {
+    return (
+        <>
+        <h1>Sorry, page not found.</h1>
+        <p>Please check your information and try again.</p>
+        </>
+    )
+}
